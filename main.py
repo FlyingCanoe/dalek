b9fa323a8ae5f5db945a0faa4b2c004e91e5c4c9
UTILISA-43TGJ9A\admin1 <bthuot@cvm.qc.ca>
2021-09-15 10:09:55 -0400

    example prof2

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import random
+import os
+import sys
+
 
 class Vue:
     def __init__(self, parent):
@@ -9,30 +12,97 @@ class Vue:
         rep=input("Que voulez-vous, 1-pour partie, 2-pour score\n")
         self.parant.demande_initiale(rep)
 
+    def affcher_partie(self, partie):
+        matrice = []
+        for y in range(partie.dimy):
+            ligne = []
+            for x in range(partie.dimx):
+                ligne.append("-")
+            matrice.append(ligne)
+
+        docx = partie.doc.pos[0]
+        docy = partie.doc.pos[1]
+
+        matrice[docy][docx] = "O"
+
+        for dalek in partie.daleks:
+            dalekx = dalek.pos[0]
+            daleky = dalek.pos[1]
+
+            matrice[daleky][dalekx] = "W"
+
+        for ligne in matrice:
+            print(ligne)
+
+
+class Dalek:
+    def __init__(self, parant, pos):
+        self.parant = parant
+        self.pos = pos
+
+
 class Jeu:
     def __init__(self, parent):
-        self.partie = Partie()
+        self.partie = None
         self.parant=parent
         self.nbr_dalek_par_niveau=5
 
-class Partie():
-    def __init__(self):
+    def crée_partie(self):
+        self.partie = Partie(self)
+        self.partie.crée_niveau()
+
+
+class Partie:
+    def __init__(self, parent):
+        self.parant = parent
         self.doc=None
-        self.largeur=12
-        self.hauteur=8
+        self.dimx=12
+        self.dimy=8
         self.niveau=0
         self.daleks=[]
+        self.ferrailles = []
+
+    def crée_niveau(self):
+        self.niveau += 1
+        nbr_daleks = self.niveau+self.parant.nbr_dalek_par_niveau
+
+        posx = random.randrange(self.dimx)
+        posy = random.randrange(self.dimy)
+        nbr_pos = [[posx, posy]]
+        nerreur = 0
+        while len(nbr_pos) <= nbr_daleks + 1:
+            posx = random.randrange(self.dimx)
+            posy = random.randrange(self.dimy)
+            if [posx, posy] not in nbr_pos:
+                nbr_pos.append([posx, posy])
+            else:
+                nerreur += 1
+        posdoc = nbr_pos.pop(0)
+        self.doc = Docteur(self, posdoc)
+
+        for pos in nbr_pos:
+            self.daleks.append(Dalek(self, pos))
+
+        self.ferrailles = []
+
+
+class Docteur:
+    def __init__(self, parent, pos):
+        self.parent = parent
+        self.pos = pos
+
 
 class Controleur:
     def __init__(self):
-        self.modele=Jeu(self)
-        self.vue=Vue(self)
+        self.modele = Jeu(self)
+        self.vue = Vue(self)
 
         self.vue.afficher_menu_initial()
 
     def demande_initiale(self, rep):
-        print(rep)
+        self.modele.crée_partie()
+        self.vue.affcher_partie(self.modele.partie)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     controler = Controleur()
